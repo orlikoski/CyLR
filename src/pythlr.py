c0e3c5f101c03d14562e1f0820fc654800a83f03
Rough007 <not@home.now>
2016-09-09 21:27:45 -0600

    Beta, more updates

--- src/pythlr.py
+++ src/pythlr.py
@@ -1,5 +1,14 @@
 #!python3
 import io, os, sys, argparse, subprocess, csv, time, datetime, re, multiprocessing, shutil, tempfile, zipfile
+try:
+    import zlib
+    compression = zipfile.ZIP_DEFLATED
+except:
+    compression = zipfile.ZIP_STORED
+
+modes = { zipfile.ZIP_DEFLATED: 'deflated',
+          zipfile.ZIP_STORED:   'stored',
+          }
 ###############################################################################
 # Created by: Alan Orlikoski
 # Version Info
@@ -13,59 +22,41 @@ import io, os, sys, argparse, subprocess, csv, time, datetime, re, multiprocessi
 ###############################################################################
 
 
-# Sample usage
+rawcopy=r"rawcopy.exe"
+rawcopy64=r"rawcopy64.exe"
 
-# Example for copying the pagefile off a running system
-# RawCopy.exe /FileNamePath:C:\pagefile.sys /OutputPath:E:\output
-
-# Example for copying the SYSTEM hive off a running system
-# RawCopy.exe /FileNamePath:C:\WINDOWS\system32\config\SYSTEM /OutputPath:E:\output
-
-# Example for extracting the $MFT by specifying its index number, into to the program directory.
-# RawCopy.exe /FileNamePath:C:0
-
-# Example for extracting MFT reference number 30224 and all attributes including $DATA, and dumping it into C:\tmp:
-# RawCopy.exe /FileNamePath:C:30224 /OutputPath:C:\tmp /AllAttr:1
-
-# Example for accessing a disk image and extracting MftRef ($LogFile) from volume number 2.
-# RawCopy.exe /ImageFile:e:\temp\diskimage.dd /ImageVolume:2 /FileNamePath:c:2 /OutputPath:e:\out
-
-# Example for accessing partition/volume image and extracting file.ext and dumping it into E:\out.
-# RawCopy.exe /ImageFile:e:\temp\partimage.dd /ImageVolume:1 /FileNamePath:c:\file.ext /OutputPath:e:\out
-
-# Example for making a raw dirlisting in detailed mode in c:\$Extend:
-# RawCopy.exe /FileNamePath:c:\$Extend /RawDirMode:1
-
-# Example for making a raw dirlisting in basic mode in c:\System Volume Information inside a disk image file:
-# RawCopy.exe /ImageFile:e:\temp\diskimage.dd /ImageVolume:1 /FileNamePath:"c:\System Volume Information" /RawDirMode:2
-
-# Example for making a raw dirlisting in detailed mode on the root level inside a shadow copy:
-# RawCopy.exe /FileNamePath:\\.\HarddiskVolumeShadowCopy1:x:\ /RawDirMode:1
-
-# Example for extracting $MFT from partition 2 on harddisk 1 and dumping it into e:\out:
-# RawCopy.exe /FileNamePath:\\.\Harddisk0Partition2:0 /OutputPath:e:\out
-
-# Example for extracting $MFT from second volume on PhysicalDrive0:
-# RawCopy.exe /FileNamePath:\\.\PhysicalDrive0:0 /ImageVolume:2 /OutputPath:e:\out
-rawcopy=r"F:\GitHub\Build\Pythlr\Beta\rawcopy.exe"
-rawcopy64=r"F:\GitHub\Build\Pythlr\Beta\rawcopy64.exe"
-
-#filenamepath=r"C:\mtmp\test.txt"
 outputpath=tempfile.TemporaryDirectory(prefix="LRDATADIR_")
-print("Created Directory", outputpath)
-outputzip=r"F:\GitHub\Build\Pythlr\Beta\data\LRdata.zip"
+outputzip=r"LRdata.zip"
 
 
 
 # Complete list of artifacts (MFT must be last for some reason or it doesn't work)
-LR_artifacts = [r"%SystemRoot%\System32\config",
-                r"%SystemRoot%\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup",
-                r"%SystemRoot%\Prefetch",
-                r"%SystemRoot%\Windows\Tasks",
-                r"%SystemRoot%\Windows\SchedLgU.Txt",
-                r"%SystemRoot%\System32\winevt\logs",
+# Everything for LR
+# LR_artifacts = [r"%SystemRoot%\System32\config",
+#                 r"%SystemRoot%\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup",
+#                 r"%SystemRoot%\Prefetch",
+#                 r"%SystemRoot%\Tasks",
+#                 r"%SystemRoot%\SchedLgU.Txt",
+#                 r"%SystemRoot%\System32\winevt\logs",
+#                 r"%SystemRoot%\System32\drivers\etc\hosts",
+#                 r"C:0"]
+
+# Laser Focused LR
+LR_artifacts = [r"%SystemRoot%\System32\config\SAM",
+                r"%SystemRoot%\System32\config\SECURITY",
+                r"%SystemRoot%\System32\config\SOFTWARE",
+                r"%SystemRoot%\System32\config\SYSTEM",
+                r"%SystemRoot%\System32\winevt\logs\Application.evtx",
+                r"%SystemRoot%\System32\winevt\logs\Security.evtx",
+                r"%SystemRoot%\System32\winevt\logs\System.evtx",
+                r"%SystemRoot%\System32\winevt\logs\Windows PowerShell.evtx",
+                r"%SystemRoot%\Tasks",
+                r"%SystemRoot%\SchedLgU.Txt",
                 r"%SystemRoot%\System32\drivers\etc\hosts",
                 r"C:0"]
+
+# Testing LR
+# LR_artifacts = [r"C:0"]
 
 
 # Function to copy file to a directory
@@ -74,18 +65,34 @@ def get_file(filenamepath):
         if not filenamepath == r"C:0":
             print("File does not exist:", filenamepath)
             return
-    print("Collecting file:", filenamepath)
+    if not filenamepath == r"C:0":
+        print("Collecting file:", filenamepath)
+    else:
+        print("Collecting file: $MFT")
+    sourcepath = filenamepath[2:]
+    if sourcepath[0] != "\\":
+        copypath = outputpath + "\\"+ os.path.dirname(sourcepath)
+    else:
+        copypath = outputpath + os.path.dirname(sourcepath)
+
+    if not os.path.exists(copypath):
+        os.makedirs(copypath)
+
+
     try:
-        subprocess.call([rawcopy64,r"/FileNamePath:"+filenamepath,r"/OutputPath:"+outputpath])
+        subprocess.call([rawcopy64,r"/FileNamePath:"+filenamepath,r"/OutputPath:"+copypath])
     except:
-        subprocess.call([rawcopy,r"/FileNamePath:"+filenamepath,r"/OutputPath:"+outputpath])
-    filename = outputpath+"\\"+os.path.basename(filenamepath)
-    mftname = outputpath+"\\"+"$MFT"
+        subprocess.call([rawcopy,r"/FileNamePath:"+filenamepath,r"/OutputPath:"+copypath])
+    filename = copypath+"\\"+os.path.basename(filenamepath)
+    mftname = copypath+"$MFT"
+
     while not os.path.exists(filename) and not os.path.exists(mftname):
         time.sleep(1)
     if filenamepath == r"C:0":
-        filenamepath = "$MFT"
-    print("File collected:")#" ", filenamepath)
+        print("File written to:", mftname)
+    else:
+        print("File written to:", filename)
+    print("")
     return
 
 # Function to copy all files in directory and sub-directories to a directory
@@ -96,8 +103,10 @@ def get_dir(dirnamepath):
                 get_file(filenamepath)
     return
 
+print("Collection process started")
 
 with tempfile.TemporaryDirectory(prefix="LRDATADIR_") as outputpath:
+    print("Created Temp Dir:", outputpath)
     for artifact in LR_artifacts:
         exp_artifact = os.path.expandvars(artifact)
         if os.path.isdir(exp_artifact):
@@ -108,18 +117,15 @@ with tempfile.TemporaryDirectory(prefix="LRDATADIR_") as outputpath:
     if os.path.isfile(outputzip):
         print("deleting existing file:", outputzip)
         os.remove(outputzip)
-    print("Compressing artifacts into:", outputzip)
+    print("Compressing artifacts")
     zf = zipfile.ZipFile(outputzip, "w")
     for dirname, subdirs, files in os.walk(outputpath):
-        zf.write(dirname)
         for filename in files:
-            zf.write(os.path.join(dirname, filename))
+            fnameandpath = os.path.join(dirname, filename)
+            zf.write(fnameandpath, os.path.relpath(fnameandpath, outputpath), compress_type=compression)
     zf.close()
     print("Compression complete")
 
-
-
-
-
-
-print("Process Complete: LR DATA file is:", outputzip)
+print("Deleted Temp Dir:", outputpath)
+print("LR DATA file is:", outputzip)
+print("Collection process complete")
