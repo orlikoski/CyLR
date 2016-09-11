3fa69fa578d44eafe1ab55a584d0c9f825e2fe5b
Rough007 <not@home.now>
2016-09-11 13:33:45 -0600

    Added Multi-threading and other optimizations

--- src/pythlr.py
+++ src/pythlr.py
@@ -1,5 +1,8 @@
 #!python3
-import io, os, sys, argparse, subprocess, csv, time, datetime, re, multiprocessing, shutil, tempfile, zipfile
+import io, os, sys, argparse, subprocess, time, datetime, re, tempfile, zipfile, threading, shutil
+#from multiprocessing.pool import Pool
+
+
 try:
     import zlib
     compression = zipfile.ZIP_DEFLATED
@@ -21,46 +24,63 @@ modes = { zipfile.ZIP_DEFLATED: 'deflated',
 
 ###############################################################################
 
+MFTREF_pattern = re.compile('^MFT Ref: (\d.{1,20})')
+MFTFileName_pattern = re.compile('FileName: (\S.{1,200})')
 
 rawcopy=r"rawcopy.exe"
 rawcopy64=r"rawcopy64.exe"
 
-outputpath=tempfile.TemporaryDirectory(prefix="LRDATADIR_")
+outputpath="LRData"
 outputzip=r"LRdata.zip"
+max_cpu = True
 
 
 
 # Complete list of artifacts (MFT must be last for some reason or it doesn't work)
 # Everything for LR
-# LR_artifacts = [r"%SystemRoot%\System32\config",
-#                 r"%SystemRoot%\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup",
-#                 r"%SystemRoot%\Prefetch",
-#                 r"%SystemRoot%\Tasks",
-#                 r"%SystemRoot%\SchedLgU.Txt",
-#                 r"%SystemRoot%\System32\winevt\logs",
-#                 r"%SystemRoot%\System32\drivers\etc\hosts",
-#                 r"C:0"]
-
-# Laser Focused LR
-LR_artifacts = [r"%SystemRoot%\System32\config\SAM",
-                r"%SystemRoot%\System32\config\SECURITY",
-                r"%SystemRoot%\System32\config\SOFTWARE",
-                r"%SystemRoot%\System32\config\SYSTEM",
-                r"%SystemRoot%\System32\winevt\logs\Application.evtx",
-                r"%SystemRoot%\System32\winevt\logs\Security.evtx",
-                r"%SystemRoot%\System32\winevt\logs\System.evtx",
-                r"%SystemRoot%\System32\winevt\logs\Windows PowerShell.evtx",
+LR_artifacts = [r"%SystemRoot%\System32\config",
+                r"%SystemRoot%\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup",
+                r"%SystemRoot%\Prefetch",
                 r"%SystemRoot%\Tasks",
                 r"%SystemRoot%\SchedLgU.Txt",
+                r"%SystemRoot%\System32\winevt\logs",
                 r"%SystemRoot%\System32\drivers\etc\hosts",
                 r"C:0"]
 
+# Laser Focused LR
+# LR_artifacts = [r"%SystemRoot%\System32\config\SAM",
+#                 r"%SystemRoot%\System32\config\SECURITY",
+#                 r"%SystemRoot%\System32\config\SOFTWARE",
+#                 r"%SystemRoot%\System32\config\SYSTEM",
+#                 r"%SystemRoot%\System32\winevt\logs\Application.evtx",
+#                 r"%SystemRoot%\System32\winevt\logs\Security.evtx",
+#                 r"%SystemRoot%\System32\winevt\logs\System.evtx",
+#                 r"%SystemRoot%\System32\winevt\logs\Windows PowerShell.evtx",
+#                 r"%SystemRoot%\Tasks",
+#                 r"%SystemRoot%\SchedLgU.Txt",
+#                 r"%SystemRoot%\System32\drivers\etc\hosts",
+#                 r"C:0"]
+
 # Testing LR
-# LR_artifacts = [r"C:0"]
+# LR_artifacts = [r"%SystemRoot%\System32\config"]
+
+# Call RawCopy command
+def rawcopyfunc(filenamepath,copypath):
+    command64bit = [rawcopy64,r"/FileNamePath:"+filenamepath,r"/OutputPath:"+copypath]
+    command32bit = [rawcopy,r"/FileNamePath:"+filenamepath,r"/OutputPath:"+copypath]
+
+    try:
+        subprocess.call(command64bit,stdout=subprocess.DEVNULL)
+        print("The command run was:"," ".join(command64bit))
+
+    except:
+        subprocess.call(command32bit,stdout=subprocess.DEVNULL)
+        print("The command run was:"," ".join(command32bit))
+    return
 
 
 # Function to copy file to a directory
-def get_file(filenamepath):
+def get_file(filenamepath, copypath):
     if not os.path.isfile(filenamepath):
         if not filenamepath == r"C:0":
             print("File does not exist:", filenamepath)
@@ -69,20 +89,12 @@ def get_file(filenamepath):
         print("Collecting file:", filenamepath)
     else:
         print("Collecting file: $MFT")
-    sourcepath = filenamepath[2:]
-    if sourcepath[0] != "\\":
-        copypath = outputpath + "\\"+ os.path.dirname(sourcepath)
-    else:
-        copypath = outputpath + os.path.dirname(sourcepath)
 
     if not os.path.exists(copypath):
         os.makedirs(copypath)
 
+    rawcopyfunc(filenamepath,copypath)
 
-    try:
-        subprocess.call([rawcopy64,r"/FileNamePath:"+filenamepath,r"/OutputPath:"+copypath])
-    except:
-        subprocess.call([rawcopy,r"/FileNamePath:"+filenamepath,r"/OutputPath:"+copypath])
     filename = copypath+"\\"+os.path.basename(filenamepath)
     mftname = copypath+"$MFT"
 
@@ -95,37 +107,81 @@ def get_file(filenamepath):
     print("")
     return
 
-# Function to copy all files in directory and sub-directories to a directory
-def get_dir(dirnamepath):
-    for dirname, subdirs, files in os.walk(dirnamepath):
-            for filename in files:
-                filenamepath =os.path.join(dirname, filename)
-                get_file(filenamepath)
-    return
+# Function to return a list of the MFT numbers for each file in a directory
+def get_dir(dirnamepath, copypath):
+    print("Parsing the MFT for all files in:", dirnamepath)
+    dp = subprocess.Popen([rawcopy64,r"/FileNamePath:"+dirnamepath,r"/RawDirMode:1"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    fileinfo = []
+    lof = []
+    for line in io.TextIOWrapper(dp.stdout, encoding="utf-8"):
+        MFTFileName = MFTFileName_pattern.search(line)
+        if MFTFileName:
+            fileinfo.append(MFTFileName.group(1))
 
-print("Collection process started")
+        MFTREF = MFTREF_pattern.search(line)
+        if MFTREF:
+            fileinfo.append(MFTREF.group(1))
+            lof.append(fileinfo)
+            fileinfo = []
 
-with tempfile.TemporaryDirectory(prefix="LRDATADIR_") as outputpath:
-    print("Created Temp Dir:", outputpath)
-    for artifact in LR_artifacts:
-        exp_artifact = os.path.expandvars(artifact)
-        if os.path.isdir(exp_artifact):
-            get_dir(exp_artifact)
+    threads = []
+    for filerecords in lof:
+        print("Collecting file:",dirnamepath+"\\"+filerecords[0])
+        outputpath = copypath+"\\"+os.path.basename(dirnamepath)
+        if not os.path.exists(outputpath):
+            os.makedirs(outputpath)
+        if max_cpu:
+            t = threading.Thread(target=rawcopyfunc, args=("C:"+filerecords[1],outputpath))
+            threads.append(t)
+            t.start()
         else:
-            get_file(exp_artifact)
-    print("All artifacts collected")
-    if os.path.isfile(outputzip):
-        print("deleting existing file:", outputzip)
-        os.remove(outputzip)
-    print("Compressing artifacts")
-    zf = zipfile.ZipFile(outputzip, "w")
-    for dirname, subdirs, files in os.walk(outputpath):
-        for filename in files:
-            fnameandpath = os.path.join(dirname, filename)
-            zf.write(fnameandpath, os.path.relpath(fnameandpath, outputpath), compress_type=compression)
-    zf.close()
-    print("Compression complete")
+            rawcopyfunc("C:"+filerecords[1],outputpath)
 
-print("Deleted Temp Dir:", outputpath)
-print("LR DATA file is:", outputzip)
-print("Collection process complete")
+    # Wait for all threads to finish
+    for t in threads:
+        t.join()
+
+
+def main():
+    ts = time.time()
+    print("Collection process started")
+
+    # Creating temp directory and gathering artifacts
+    with tempfile.TemporaryDirectory(prefix="LRDATADIR_") as outputpath:
+        print("Created Temp Dir:", outputpath)
+        for artifact in LR_artifacts:
+            exp_artifact = os.path.expandvars(artifact)
+            sourcepath = exp_artifact[2:]
+            if sourcepath[0] != "\\":
+                copypath = outputpath + "\\"+ os.path.dirname(sourcepath)
+            else:
+                copypath = outputpath + os.path.dirname(sourcepath)
+
+            if os.path.isdir(exp_artifact):
+                get_dir(exp_artifact,copypath)
+            else:
+                get_file(exp_artifact,copypath)
+        print("All artifacts collected")
+
+
+        # Compressing Results
+        if os.path.isfile(outputzip):
+            print("deleting existing file:", outputzip)
+            os.remove(outputzip)
+        print("Compressing artifacts")
+        zf = zipfile.ZipFile(outputzip, "w")
+        for dirname, subdirs, files in os.walk(outputpath):
+            for filename in files:
+                fnameandpath = os.path.join(dirname, filename)
+                zf.write(fnameandpath, os.path.relpath(fnameandpath, outputpath), compress_type=compression)
+        zf.close()
+        print("Compression complete")
+
+    # Wrapping it up
+    print("Deleted Temp Dir:", outputpath)
+    print("LR DATA file is:", outputzip)
+    print("Collection process complete")
+    print('Took {}s'.format(time.time() - ts))
+
+if __name__ == '__main__':
+   main()
