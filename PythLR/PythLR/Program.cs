ae3d022d2ba3a0bc00aea22e4686d84272179524
Jason Yegge <Lansatac@gmail.com>
2016-09-14 19:51:57 -0600

    removing the need for a new FileStream.

--- PythLR/PythLR/Program.cs
+++ PythLR/PythLR/Program.cs
@@ -42,10 +42,10 @@ namespace PythLR
             if (arguments.SFTPCheck)
             {
                 var zipPath = $@".\{Environment.MachineName}.zip";
-                files.CollectFilesToArchive(zipPath);
+                var archiveStream = files.CollectFilesToArchive(zipPath);
                 var client = new SftpClient(arguments.SFTPServer, 22, arguments.UserName, arguments.UserPassword);
                 client.Connect();
-                client.UploadFile(new FileStream(zipPath, FileMode.Open, FileAccess.Read), $@"{arguments.OutputPath}/{Environment.MachineName}.zip");
+                client.UploadFile(archiveStream, $@"{arguments.OutputPath}/{Environment.MachineName}.zip");
                 client.Disconnect();
             }
             else
