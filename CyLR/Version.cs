09efed9b3323ffa1f1367ac246e50604f4741976
Jason Yegge <Lansatac@gmail.com>
2016-09-25 22:31:53 -0600

    Help message version now uses the Assembly's version.

---
+++ CyLR/Version.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using SystemVersion = System.Version;
+
+namespace CyLR
+{
+    internal static class Version
+    {
+        public static SystemVersion GetVersion()
+        {
+            var assem = Assembly.GetEntryAssembly();
+            var assemName = assem.GetName();
+            return assemName.Version;
+        }
+    }
+}
