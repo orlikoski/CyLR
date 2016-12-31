f25a50b8c91bdd2e1b3abf29580992a6b8aeecc1
Jason Yegge <Lansatac@gmail.com>
2016-12-31 08:35:55 -0700

    Adding first unit tests.

---
+++ CyLRTests/TestArguments.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CyLR;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CyLRTests
+{
+    [TestClass]
+    public class TestArguments
+    {
+        [TestMethod]
+        public void TestValidSFTP()
+        {
+            new Arguments(new []{"-s", "test.server.com", "-u", "username", "-p", "password"});
+        }
+
+        [TestMethod]
+        public void TestSFTPMissingParams()
+        {
+            AssertUtil.ShouldThrow<ArgumentException>(
+                () => { new Arguments(new[] {"-s", "test.server.com", "-u", "username"}); }
+                );
+            AssertUtil.ShouldThrow<ArgumentException>(
+                () => { new Arguments(new[] { "-s", "test.server.com", "-p", "password" }); }
+                );
+            AssertUtil.ShouldThrow<ArgumentException>(
+                () => { new Arguments(new[] { "-u", "username", "-p", "password" }); }
+                );
+        }
+    }
+}
