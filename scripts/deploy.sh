93cdb78427c081d07027fbf00c4554d8330a7c23
Jason Yegge <Lansatac@gmail.com>
2017-01-14 17:19:26 -0700

    travis is hating on the scripts

--- scripts/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PLATFORM=$1
-
-zip -j ./CyLR/bin/merged/CyLR$PLATFORM.zip ./CyLR/bin/merged/CyLR.exe
--- scripts/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-PLATFORM=$1
-
-mono ./packages/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe CyLRTests/bin/Release/CyLRTests.dll
