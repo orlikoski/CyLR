201fe1ac8143b0e7bf278ea21787dafd04bb6f1b
Alan Orlikoski <alan@orlikoski.com>
2018-10-20 21:30:38 -0500

    tuning build script

--- CyLR/build_items/scripts/build.sh
+++ CyLR/build_items/scripts/build.sh
@@ -18,9 +18,9 @@ chmod +x warp-packer
 # Start Build Process
 dotnet restore
 
-platforms=( "linux-x64" "osx-x64" "win-x86" "win-x64" )
+platforms_win=( "linux-x64" "osx-x64" )
 
-# Build and Publish Each Version
+# Build and Publish Linux and MacOs Versions
 for platform in "${platforms[@]}"
 do
   deployments_dir="$deployment_root_dir/$platform"
@@ -29,5 +29,18 @@ do
   dotnet test $test_dir
   dotnet build -c release -r $platform
   dotnet publish -c release -r $platform
-  ./warp-packer --arch $platform --input_dir bin/Release/netcoreapp$netcore_version/linux-x64/publish --exec CyLR.exe --output $deployments_dir/CyLR.exe
+  ./warp-packer --arch $platform --input_dir bin/release/netcoreapp$netcore_version/linux-x64/publish --exec CyLR --output $deployments_dir/CyLR
+done
+
+platforms_win=( "win-x86" "win-x64" )
+# Build and Publish Windows Versions
+for platform in "${platforms[@]}"
+do
+  deployments_dir="$deployment_root_dir/$platform"
+  mkdir -p $deployments_dir
+
+  dotnet test $test_dir
+  dotnet build -c release -r $platform
+  dotnet publish -c release -r $platform
+  ./warp-packer --arch $platform --input_dir bin/release/netcoreapp$netcore_version/linux-x64/publish --exec CyLR.exe --output $deployments_dir/CyLR.exe
 done
