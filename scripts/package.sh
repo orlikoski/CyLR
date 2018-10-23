c147f174d3a11c0a5fa9bd0ea853fb344da756ae
Jason Yegge <Lansatac@gmail.com>
2018-10-22 21:05:52 -0600

    adding debug logging

--- scripts/package.sh
+++ scripts/package.sh
@@ -6,12 +6,14 @@ BUILD_DIR="." && [[ "$TRAVIS_BUILD_DIR" != "" ]] && BUILD_DIR=$TRAVIS_BUILD_DIR
 mkdir $BUILD_DIR/deployments
 
 if [ USE_WARP = true ] ; then
-
+	echo "Packaging build with Warp"
 	curl -Lo $BUILD_DIR/warp-packer https://github.com/dgiagio/warp/releases/download/v0.2.1/$WARP_ARCH.warp-packer \
 		&& chmod +x $BUILD_DIR/warp-packer \
 		&& $BUILD_DIR/warp-packer --arch $WARP_ARCH --input_dir $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ --exec CyLR --output $BUILD_DIR/deployments/CyLR
+	echo "Warp complete."
 else
 	cp -r $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ $BUILD_DIR/deployments/CyLR
 fi
-
+echo "Zipping files:"
+ls -R $BUILD_DIR/deployments
 zip -j $BUILD_DIR/CyLR_$BUILD_ARCH.zip $BUILD_DIR/deployments/*
