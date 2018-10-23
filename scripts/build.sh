56dea6b9371c99794c86e373c59fa640bb10007a
Jason Yegge <Lansatac@gmail.com>
2018-10-22 19:33:06 -0600

    Splitting build into stages.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,5 @@
-BUILD_DIR=.
-
-[[ "$BUILD_ARCH" == "linux-x64" ]] && WARP_ARCH = linux-x64
-[[ "$BUILD_ARCH" == "osx-x64" ]] && WARP_ARCH = macos-x64
-
+BUILD_DIR=$TRAVIS_BUILD_DIR
 
 dotnet restore \
 	&& dotnet build -c release -r $BUILD_ARCH \
 	&& dotnet publish -c release -r $BUILD_ARCH
-
-
-mkdir $BUILD_DIR/deployments
-
-curl -Lo $BUILD_DIR/warp-packer https://github.com/dgiagio/warp/releases/download/v0.2.1/$WARP_ARCH.warp-packer \
-	&& chmod +x $BUILD_DIR/warp-packer \
-	&& $BUILD_DIR/warp-packer --arch $BUILD_ARCH --input_dir $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ --exec CyLR --output $BUILD_DIR/deployments/CyLR
-
-zip -j $BUILD_DIR/CyLR_$BUILD_ARCH.zip $BUILD_DIR/deployments/CyLR
---
+++ scripts/test.sh
@@ -0,0 +1,4 @@
+BUILD_DIR=$TRAVIS_BUILD_DIR
+
+dotnet restore \
+	&& dotnet test
