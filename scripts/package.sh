91e12d183fdaf5cb2bf88322bde492d0394ac309
Jason Yegge <Lansatac@gmail.com>
2018-10-22 20:16:42 -0600

    Fixing warp arch

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,6 +7,6 @@ mkdir $BUILD_DIR/deployments
 
 curl -Lo $BUILD_DIR/warp-packer https://github.com/dgiagio/warp/releases/download/v0.2.1/$WARP_ARCH.warp-packer \
 	&& chmod +x $BUILD_DIR/warp-packer \
-	&& $BUILD_DIR/warp-packer --arch $BUILD_ARCH --input_dir $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ --exec CyLR --output $BUILD_DIR/deployments/CyLR
+	&& $BUILD_DIR/warp-packer --arch $WARP_ARCH --input_dir $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ --exec CyLR --output $BUILD_DIR/deployments/CyLR
 
 zip -j $BUILD_DIR/CyLR_$BUILD_ARCH.zip $BUILD_DIR/deployments/CyLR
