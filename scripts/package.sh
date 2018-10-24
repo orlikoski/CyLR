601fa75c7a3094cc3d8618802342884bdf485bc9
Jason Yegge <Lansatac@gmail.com>
2018-10-23 21:15:12 -0600

    fixing warp commands

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,14 +4,18 @@ BUILD_DIR="." && [[ "$TRAVIS_BUILD_DIR" != "" ]] && BUILD_DIR=$TRAVIS_BUILD_DIR
 
 [[ "$BUILD_ARCH" == "linux-x64" ]] && WARP_ARCH=linux-x64
 [[ "$BUILD_ARCH" == "osx-x64" ]] && WARP_ARCH=macos-x64
+[[ "$BUILD_ARCH" == "win-x64" ]] && WARP_ARCH=windows-x64
+
+WARP_COMMAND=$WARP_ARCH.warp-packer
+[[ "$BUILD_ARCH" == "win-x64" ]] && WARP_COMMAND=$WARP_ARCH.warp-packer.exe
 
 mkdir $BUILD_DIR/deployments
 
 if [ "$USE_WARP" = true ] ; then
 	echo "Packaging build with Warp"
-	curl -Lo $BUILD_DIR/warp-packer https://github.com/dgiagio/warp/releases/download/v0.2.1/$WARP_ARCH.warp-packer
-	chmod +x $BUILD_DIR/warp-packer
-	$BUILD_DIR/warp-packer --arch $WARP_ARCH --input_dir $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ --exec CyLR --output $BUILD_DIR/deployments/CyLR
+	curl -sSLo $BUILD_DIR/$WARP_COMMAND https://github.com/dgiagio/warp/releases/download/v0.2.1/$WARP_COMMAND
+	chmod +x $BUILD_DIR/$WARP_COMMAND
+	$BUILD_DIR/$WARP_COMMAND --arch $WARP_ARCH --input_dir $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ --exec CyLR --output $BUILD_DIR/deployments/CyLR
 	echo "Warp complete."
 else
 	cp -r $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ $BUILD_DIR/deployments/CyLR
