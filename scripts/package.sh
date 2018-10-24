13584e6a02056d93e17b9435a2bc7043d571e62e
Jason Yegge <Lansatac@gmail.com>
2018-10-23 21:36:32 -0600

    Fixing packaging for windows

--- scripts/package.sh
+++ scripts/package.sh
@@ -9,15 +9,16 @@ BUILD_DIR="." && [[ "$TRAVIS_BUILD_DIR" != "" ]] && BUILD_DIR=$TRAVIS_BUILD_DIR
 WARP_COMMAND=$WARP_ARCH.warp-packer
 [[ "$BUILD_ARCH" == "win-x64" ]] && WARP_COMMAND=$WARP_ARCH.warp-packer.exe
 
+CYLR_EXE="CyLR" && [[ "$BUILD_ARCH" == "win-x64" ]] && CYLR_EXE="CyLR.exe"
+
 mkdir $BUILD_DIR/deployments
 
 if [ "$USE_WARP" = true ] ; then
 	echo "Packaging build with Warp" \
 		&& curl -sSLo $BUILD_DIR/$WARP_COMMAND https://github.com/dgiagio/warp/releases/download/v0.2.1/$WARP_COMMAND \
 		&& chmod +x $BUILD_DIR/$WARP_COMMAND \
-		&& $BUILD_DIR/$WARP_COMMAND --arch $WARP_ARCH --input_dir $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ --exec CyLR --output $BUILD_DIR/deployments/CyLR \
+		&& $BUILD_DIR/$WARP_COMMAND --arch $WARP_ARCH --input_dir $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ --exec $CYLR_EXE --output $BUILD_DIR/deployments/$CYLR_EXE \
 		&& echo "Warp complete."
-	sleep 5
 else
 	cp -r $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ $BUILD_DIR/deployments/CyLR
 fi
