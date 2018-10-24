a5945244336d0013a5668de1fd86557a9e4c0079
Jason Yegge <Lansatac@gmail.com>
2018-10-23 21:19:41 -0600

    trying a sleep for debugging

--- scripts/package.sh
+++ scripts/package.sh
@@ -14,6 +14,7 @@ mkdir $BUILD_DIR/deployments
 if [ "$USE_WARP" = true ] ; then
 	echo "Packaging build with Warp"
 	curl -sSLo $BUILD_DIR/$WARP_COMMAND https://github.com/dgiagio/warp/releases/download/v0.2.1/$WARP_COMMAND
+	sleep 5
 	chmod +x $BUILD_DIR/$WARP_COMMAND
 	$BUILD_DIR/$WARP_COMMAND --arch $WARP_ARCH --input_dir $BUILD_DIR/CyLR/bin/Release/netcoreapp2.1/$BUILD_ARCH/publish/ --exec CyLR --output $BUILD_DIR/deployments/CyLR
 	echo "Warp complete."
