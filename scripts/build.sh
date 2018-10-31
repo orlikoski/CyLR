8734b0b222616ebf27c227371b3adf3c37725a91
Jason Yegge <Lansatac@gmail.com>
2018-10-30 20:44:00 -0600

    switching to non-single exe

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 BUILD_DIR="." && [[ "$TRAVIS_BUILD_DIR" != "" ]] && BUILD_DIR=$TRAVIS_BUILD_DIR
 
 dotnet restore \
-	&& dotnet publish -c release -r $BUILD_ARCH
+	&& dotnet publish -f netcoreapp2.1 -c release -r $BUILD_ARCH
