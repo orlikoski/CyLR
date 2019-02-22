1abb4334bf3447d2e39a163c145a43cb5f0895a0
Alan Orlikoski <alan@orlikoski.com>
2019-02-22 11:56:03 -0600

    update

--- scripts/package.sh
+++ scripts/package.sh
@@ -14,7 +14,7 @@ CYLR_EXE="CyLR" && [[ "$BUILD_ARCH" == "win-x64" ]] && CYLR_EXE="CyLR.exe"
 
 mkdir -p $BUILD_DIR/deployments
 
-if [ "$USE_WARP" = TRUE ] ; then
+if [ "$USE_WARP" = true ] ; then
 	echo "Packaging build with Warp" \
 		&& curl -sSLo $BUILD_DIR/$WARP_COMMAND https://github.com/dgiagio/warp/releases/download/v0.3.0/$WARP_COMMAND \
 		&& chmod +x $BUILD_DIR/$WARP_COMMAND \
@@ -29,6 +29,8 @@ mkdir -p "$BUILD_DIR/archive"
 
 if [ $BUILD_ARCH = "linux-x64" ] || [ $BUILD_ARCH = "osx-x64" ] ; then
 	$ZIP_COMMAND $BUILD_DIR/archive/CyLR_$BUILD_ARCH.zip $BUILD_DIR/deployments/CyLR/CyLR
+elif [ $BUILD_ARCH = "win-x64" ] ; then
+	$ZIP_COMMAND $BUILD_DIR/archive/CyLR_$BUILD_ARCH.zip --output $BUILD_DIR/deployments/$CYLR_EXE
 else
-	$ZIP_COMMAND $BUILD_DIR/archive/CyLR_$BUILD_ARCH.zip $BUILD_DIR/deployments/CyLR/publish/*
+	$ZIP_COMMAND $BUILD_DIR/archive/CyLR_$BUILD_ARCH.zip $BUILD_DIR/deployments/CyLR/*
 fi
