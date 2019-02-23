d33c3f5a75cc97258993804038e44f115fddf6c4
Alan Orlikoski <alan@orlikoski.com>
2019-02-22 19:36:45 -0600

    update

--- scripts/build_win.ps1
+++ scripts/build_win.ps1
@@ -1,8 +1,4 @@
-if (Test-Path env:TRAVIS_BUILD_DIR) {
-	$BUILD_DIR = $env:TRAVIS_BUILD_DIR
-}else {
-	$BUILD_DIR = "."
-}
-$env:BUILD_ARCH="win-x64"
+$BUILD_ARCH="win-x64"
 
-dotnet publish -f netcoreapp2.1 -c release -r $env:BUILD_ARCH
+echo "dotnet publish -f netcoreapp2.1 -c release -r $BUILD_ARCH"
+dotnet publish -f netcoreapp2.1 -c release -r $BUILD_ARCH
--- scripts/package_win.ps1
+++ scripts/package_win.ps1
@@ -11,15 +11,15 @@ mkdir -p $BUILD_DIR/deployments/$env:BUILD_ARCH/
 echo "Packaging build with Warp"
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" ; Invoke-WebRequest https://github.com/dgiagio/warp/releases/download/v0.3.0/windows-x64.warp-packer.exe -OutFile warp-packer.exe
 
-.\warp-packer.exe --arch $WARP_ARCH --input_dir $env:BUILD_DIR/CyLR/bin/release/netcoreapp2.1/$env:BUILD_ARCH/publish --exec CyLR.exe --output $env:BUILD_DIR/deployments/$env:BUILD_ARCH/CyLR.exe
+.\warp-packer.exe --arch $WARP_ARCH --input_dir $BUILD_DIR/CyLR/bin/release/netcoreapp2.1/$env:BUILD_ARCH/publish --exec CyLR.exe --output $BUILD_DIR/deployments/$env:BUILD_ARCH/CyLR.exe
 
 echo "Zipping files:"
 mkdir -p "$BUILD_DIR/archive/"
 if (-not (test-path "$env:ProgramFiles\7-Zip\7z.exe")) {throw "$env:ProgramFiles\7-Zip\7z.exe needed"}
 set-alias sz "$env:ProgramFiles\7-Zip\7z.exe"
 
-$Source = "$env:BUILD_DIR/deployments/$env:BUILD_ARCH/CyLR.exe"
-$Target = "$env:BUILD_DIR/archive/CyLR-$env:BUILD_ARCH.zip"
+$Source = "$BUILD_DIR/deployments/$env:BUILD_ARCH/CyLR.exe"
+$Target = "$BUILD_DIR/archive/CyLR-$env:BUILD_ARCH.zip"
 
 echo "sz a -tzip -mx=9 $Target $Source"
 sz a -tzip -mx=9 $Target $Source
