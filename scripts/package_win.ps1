6c0d9d9521b9630b6af3e37466f2eba43ce86ef1
Alan Orlikoski <alan@orlikoski.com>
2019-02-22 18:52:23 -0600

    update

--- scripts/package_win.ps1
+++ scripts/package_win.ps1
@@ -10,6 +10,8 @@ mkdir -p $BUILD_DIR/deployments/$env:BUILD_ARCH/
 
 echo "Packaging build with Warp"
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" ; Invoke-WebRequest https://github.com/dgiagio/warp/releases/download/v0.3.0/windows-x64.warp-packer.exe -OutFile warp-packer.exe
+
+echo ".\warp-packer.exe --arch $WARP_ARCH --input_dir $env:BUILD_DIR/CyLR/bin/release/netcoreapp2.1/$env:BUILD_ARCH/publish --exec CyLR.exe --output $env:BUILD_DIR/deployments/$env:BUILD_ARCH/CyLR.exe"
 .\warp-packer.exe --arch $WARP_ARCH --input_dir $env:BUILD_DIR/CyLR/bin/release/netcoreapp2.1/$env:BUILD_ARCH/publish --exec CyLR.exe --output $env:BUILD_DIR/deployments/$env:BUILD_ARCH/CyLR.exe
 
 echo "Zipping files:"
