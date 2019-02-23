cc87d8578352b1ec7f304cc4a6fc26a67221d4f4
Alan Orlikoski <alan@orlikoski.com>
2019-02-22 19:15:55 -0600

    update

--- scripts/package_win.ps1
+++ scripts/package_win.ps1
@@ -1,7 +1,7 @@
 if (Test-Path env:TRAVIS_BUILD_DIR) {
-	$BUILD_DIR = $env:TRAVIS_BUILD_DIR
+	$BUILD_DIR=$env:TRAVIS_BUILD_DIR
 }else {
-	$BUILD_DIR = "."
+	$BUILD_DIR=Get-Location
 }
 $env:BUILD_ARCH="win-x64"
 $WARP_ARCH = "windows-x64"
@@ -11,7 +11,6 @@ mkdir -p $BUILD_DIR/deployments/$env:BUILD_ARCH/
 echo "Packaging build with Warp"
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" ; Invoke-WebRequest https://github.com/dgiagio/warp/releases/download/v0.3.0/windows-x64.warp-packer.exe -OutFile warp-packer.exe
 
-echo ".\warp-packer.exe --arch $WARP_ARCH --input_dir $env:BUILD_DIR/CyLR/bin/release/netcoreapp2.1/$env:BUILD_ARCH/publish --exec CyLR.exe --output $env:BUILD_DIR/deployments/$env:BUILD_ARCH/CyLR.exe"
 .\warp-packer.exe --arch $WARP_ARCH --input_dir $env:BUILD_DIR/CyLR/bin/release/netcoreapp2.1/$env:BUILD_ARCH/publish --exec CyLR.exe --output $env:BUILD_DIR/deployments/$env:BUILD_ARCH/CyLR.exe
 
 echo "Zipping files:"
