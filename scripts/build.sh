6c378bc96a67d641c7077a1874bb8f6113e0f3e4
Alan Orlikoski <alan@orlikoski.com>
2020-07-20 12:32:42 -0500

    Travis build fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,8 @@
 BUILD_DIR="." && [[ "$TRAVIS_BUILD_DIR" != "" ]] && BUILD_DIR=$TRAVIS_BUILD_DIR
 
+dotnet add package Microsoft.DotNet.ILCompiler --version 1.0.5-prerelease-00002 \
+       --source https://www.myget.org/F/dotnet/api/v3/index.json
+
 if [ "$USE_CORERT" = true ] ; then
 	dotnet publish -c release -r $BUILD_ARCH
 else
