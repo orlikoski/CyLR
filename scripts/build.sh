50ba3b7e45a1812661b1f7a54aa6bb1b3a762ff6
Alan Orlikoski <alan@orlikoski.com>
2020-07-20 13:50:36 -0500

    Updating to DotNet3.1

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@ BUILD_DIR="." && [[ "$TRAVIS_BUILD_DIR" != "" ]] && BUILD_DIR=$TRAVIS_BUILD_DIR
 if [ "$USE_CORERT" = true ] ; then
 	dotnet publish -c release -r $BUILD_ARCH
 else
-	dotnet publish -f netcoreapp2.1 -c release -r $BUILD_ARCH
+	dotnet publish -f netcoreapp3.1 -c release -r $BUILD_ARCH
 fi
