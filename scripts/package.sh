bf559b53579ef5472572d55c8af0f430dc03dddd
@AlanOrlikoski <9388572+orlikoski@users.noreply.github.com>
2021-02-02 14:32:31 -0600

    Fixing publish script

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,28 +1,13 @@
 #!/usr/bin/env bash
 BUILD_DIR="." && [[ "$TRAVIS_BUILD_DIR" != "" ]] && BUILD_DIR=$TRAVIS_BUILD_DIR
 
-$ZIP_COMMAND="zip"
+ZIP_COMMAND="zip -r -j"
 
-unameOut="$(uname -s)"
-
-case "${unameOut}" in
-	Linux*)
-		BUILD_ARCH="linux-x64"
-		PACKER_ARCH="linux-x64"
-		;;
-	Darwin*)
-		BUILD_ARCH="osx-x64"
-		PACKER_ARCH="macos-x64"
-		;;
-esac
-
-if [ ! -d $BUILD_DIR/deployments/CyLR/$BUILD_ARCH ]; then
-	mkdir -p $BUILD_DIR/deployments/CyLR/$BUILD_ARCH
-fi
+mkdir -p $BUILD_DIR/deployments
 
 cp -r $BUILD_DIR/CyLR/bin/Release/netcoreapp3.1/$BUILD_ARCH/publish/ $BUILD_DIR/deployments/CyLR
 
 echo "Zipping files:"
 mkdir -p "$BUILD_DIR/archive"
 
-$ZIP_COMMAND $BUILD_DIR/archive/CyLR_$BUILD_ARCH.zip $BUILD_DIR/deployments/CyLR/$BUILD_ARCH
+$ZIP_COMMAND $BUILD_DIR/archive/CyLR_$BUILD_ARCH.zip $BUILD_DIR/deployments/CyLR/CyLR
